d6266487a33311a6bfd48683e35877555900190e
TuNombre <tu@email.com>
2025-11-05 12:54:56 -0300

    imagen estatica en videos

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['img.youtube.com', 'i.ytimg.com', 'i.vimeocdn.com'],
+  },
 };
 
-// next.config.js
-module.exports = {
-  images: {
-    domains: ['img.youtube.com', 'i.vimeocdn.com'],
-  },
-}
-
 export default nextConfig;
-
-
